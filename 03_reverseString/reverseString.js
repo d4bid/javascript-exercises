9df74f7b217751d83ade9db49aec4d48e081ca61
d4bid <dvdcabrito@gmail.com>
2025-01-28 23:21:43 +0800

    exercise 3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    let textArray = text.split("");
+    let reversedArray = textArray.reverse();
+    let joinedArray = reversedArray.join("");
 
+    return joinedArray;
 };
 
 // Do not edit below this line
