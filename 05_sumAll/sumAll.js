2f29d5b4a288142a41797582fd27cc047f6b4608
d4bid <dvdcabrito@gmail.com>
2025-01-29 12:03:08 +0800

    exercise 5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let initialNumber = num1 > num2 ? num2 : num1;
+    let finalNumber = num1 > num2 ? num1 : num2;
+    
+    let totalSum = 0;
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
 
+    if (Number.isInteger(num1) === false || Number.isInteger(num2) === false) {
+        return "ERROR";
+    }
+
+    for (i = initialNumber; i <= finalNumber; i++) {
+        totalSum += i;
+    }
+    
+    return totalSum;
 };
 
 // Do not edit below this line
